16e700066c66ef26a4e3218b287fc325b1b339d2
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-12-19 11:10:58 +0700

    Menampilkan Data Order pada Halaman My Order

---
+++ application/views/layouts/_status.php
@@ -0,0 +1,21 @@
+<?php 
+
+	if ($status == 'waiting') {
+		$badge_status = 'badge-primary';
+		$status = 'Menunggu Pembayaran';
+	} elseif ($status == 'paid') {
+		$badge_status = 'badge-secondary';
+		$status = 'Dibayar';
+	} elseif ($status == 'delivered') {
+		$badge_status = 'badge-success';
+		$status = 'Dikirim';
+	} else {
+		$badge_status = 'badge-danger';
+		$status = 'Dibatalkan';
+	}
+
+?>
+
+<?php if ($status) : ?>
+	<span class="badge badge-pill <?= $badge_status ?>"><?= $status ?></span>
+<?php endif; ?>
--- application/views/pages/myorder/index.php
+++ application/views/pages/myorder/index.php
@@ -8,54 +8,30 @@
 				<div class="card-header">Daftar Orders</div>
 				<div class="card-body">
 					<table class="table">
-					<thead>
-						<tr>
-						<th>Nomor</th>
-						<th>Tanggal</th>
-						<th>Total</th>
-						<th>Status</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-						<td>
-							<a href="/orders-detail.html"
-							><strong> #098231</strong></a
-							>
-						</td>
-						<td>12/12/2021</td>
-						<td>Rp. 300.000,-</td>
-						<td>
-							<span class="badge badge-pill badge-warning"
-							>Menunggu Pembayaran</span
-							>
-						</td>
-						</tr>
-						<tr>
-						<td>
-							<a href="/orders-detail.html">#098221</a>
-						</td>
-						<td>10/12/2021</td>
-						<td>Rp. 300.000,-</td>
-						<td>
-							<span class="badge badge-pill badge-success"
-							>Dikirim</span
-							>
-						</td>
-						</tr>
-						<tr>
-						<td>
-							<a href="/orders-detail.html">#094531</a>
-						</td>
-						<td>22/12/2021</td>
-						<td>Rp. 300.000,-</td>
-						<td>
-							<span class="badge badge-pill badge-danger"
-							>Dibatalkan</span
-							>
-						</td>
-						</tr>
-					</tbody>
+						<thead>
+							<tr>
+							<th>Nomor</th>
+							<th>Tanggal</th>
+							<th>Total</th>
+							<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach($content as $row) : ?>
+							<tr>
+								<td>
+									<a href="<?= $row->invoice ?>"
+									><strong> #<?= $row->invoice ?></strong></a
+									>
+								</td>
+								<td><?= str_replace('-', '/', date('d-m-Y', strtotime($row->date))) ?></td>
+								<td>Rp. <?= number_format($row->total, 0, ',', '.') ?>,-</td>
+								<td>
+									<?php $this->load->view('layouts/_status', ['status' => $row->status]); ?>
+								</td>
+							</tr>
+							<?php endforeach; ?>
+						</tbody>
 					</table>
 				</div>
 			</div>
